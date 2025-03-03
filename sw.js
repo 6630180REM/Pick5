33ca0bc9fa1c5a7b61a6f45acb24d7401e8b3989
6630180REM <kevin.swenson@hotmail.com>
2025-03-03 12:00:38 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,44 @@
-const CACHE_NAME = 'app-shell-v3'; // Change version to force update
+const CACHE_NAME = 'pick5-app-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './Pick5Logo.png'
+];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll([
-                '/index.html',
-                '/manifest.json?v=2',
-                '/Pick5Logo.png?v=' + new Date().getTime()
-            ]);
-        })
-    );
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-        caches.keys().then((cacheNames) => {
-            return Promise.all(
-                cacheNames
-                    .filter((name) => name !== CACHE_NAME)
-                    .map((name) => caches.delete(name))
-            );
-        })
-    );
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
 });
 
-// Intercept fetch requests to ensure latest icons
-self.addEventListener('fetch', (event) => {
-    if (event.request.url.includes('Pick5Logo.png') || event.request.url.includes('apple-touch-icon')) {
-        event.respondWith(
-            fetch(event.request.url + '?v=' + new Date().getTime(), { cache: 'reload' })
-        );
-    } else {
-        event.respondWith(
-            caches.match(event.request).then((response) => {
-                return response || fetch(event.request);
-            })
-        );
-    }
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
