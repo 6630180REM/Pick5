11c9edda7a0cc3657c374117124ff4b74b824c3b
6630180REM <kevin.swenson@hotmail.com>
2025-03-11 09:24:01 -0500

    Create pick5-push-notifications-sw.js

---
+++ pick5-push-notifications-sw.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlFST5pkmHLNcDJfx73-bPGieWVrm_nVs",
+  authDomain: "pick5-push-notifications.firebaseapp.com",
+  projectId: "pick5-push-notifications",
+  storageBucket: "pick5-push-notifications.firebasestorage.app",
+  messagingSenderId: "622059115299",
+  appId: "1:622059115299:web:4ec6d0410ee275bcfd7296"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
