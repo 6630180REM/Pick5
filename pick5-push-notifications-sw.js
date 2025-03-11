85074ef0a39526e995e7fe2e4a2f07297a04ea72
6630180REM <kevin.swenson@hotmail.com>
2025-03-11 09:26:01 -0500

    Update pick5-push-notifications-sw.js

--- pick5-push-notifications-sw.js
+++ pick5-push-notifications-sw.js
@@ -1,3 +1,6 @@
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.10.0/firebase-messaging-compat.js');
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -14,4 +17,15 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message: ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = { body: payload.notification.body };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
