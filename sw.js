262b5cbd8cd1d740d1c63ef0ed3a614527f11fce
6630180REM <kevin.swenson@hotmail.com>
2025-03-12 12:33:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,24 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js');
 
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: "AIzaSyB5r_KL2eKVFd66VQU_5pznKrVHa9xzCfc",
     authDomain: "joespick5push.firebaseapp.com",
     projectId: "joespick5push",
     storageBucket: "joespick5push.firebasestorage.app",
     messagingSenderId: "783669343677",
     appId: "1:783669343677:web:c1f72d2a2a58b0349a4ed4"
-});
+};
 
+const app = firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// Handle background messages
-messaging.onBackgroundMessage((payload) => {
-    console.log('[Service Worker] Background message:', payload);
-
+messaging.onBackgroundMessage(function(payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: 'Pick5Logo.png'
+        icon: payload.notification.icon,
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
