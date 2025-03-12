fa2d0ec2e412384afba0017bcc2b2afcb1109b7b
6630180REM <kevin.swenson@hotmail.com>
2025-03-12 15:18:26 -0500

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+// firebase-messaging-sw.js
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB5r_KL2eKVFd66VQU_5pznKrVHa9xzCfc",
+  authDomain: "joespick5push.firebaseapp.com",
+  projectId: "joespick5push",
+  storageBucket: "joespick5push.firebasestorage.app",
+  messagingSenderId: "783669343677",
+  appId: "1:783669343677:web:c1f72d2a2a58b0349a4ed4"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/Pick5Logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
