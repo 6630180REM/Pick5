c2156391a36aa49c2291e32b78a2fa4d5e540eb3
6630180REM <kevin.swenson@hotmail.com>
2025-03-12 15:31:39 -0500

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,13 +4,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.0.2/firebase-messaging.js');
 
 // Firebase configuration (use your Firebase config here)
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
-  measurementId: 'YOUR_MEASUREMENT_ID'
+  apiKey: "AIzaSyB5r_KL2eKVFd66VQU_5pznKrVHa9xzCfc",
+  authDomain: "joespick5push.firebaseapp.com",
+  projectId: "joespick5push",
+  storageBucket: "joespick5push.firebasestorage.app",
+  messagingSenderId: "783669343677",
+  appId: "1:783669343677:web:c1f72d2a2a58b0349a4ed4"
 };
 
 firebase.initializeApp(firebaseConfig);
